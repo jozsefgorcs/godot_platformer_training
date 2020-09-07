762c64396305147d8637cbd2b3bf4b903b3089a7
Jozsef Gorcs <joeygeek@gmail.com>
2020-09-07 19:55:24 +0200

    fix coin pickup

--- Items/Coin.gd
+++ Items/Coin.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 var taken = false
 
-func _on_Area2D_body_exited(body):
+
+func die():
+	queue_free()
+
+
+func _on_Area2D_body_entered(body):
 	if taken:
 		return 
 	$AnimationPlayer.play("die")
 	$AudioStreamPlayer2D.play()
 	get_tree().call_group("Gamestate", "coins_up")
 	taken = true
-	
-
-func die():
-	queue_free()
